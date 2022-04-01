bfe97ce6153b015e222bfb70e749e2a6ca1615ff
Fakhrurazi <fakhru@devstack.com.my>
2022-04-02 01:30:23 +0800

    Search feature. Grid layout. Btter ui

--- src/components/Rick.vue
+++ src/components/Rick.vue
@@ -4,17 +4,20 @@ import * as Vue from 'vue' // in Vue 3
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-let API = 'https://rickandmortyapi.com/api/character/2,435,13,44,112';
+let API = 'https://rickandmortyapi.com/api/character';
 export default {
   data() {
     return {
       rickAPI: false,
+      rickName:'',
     }
   },
   methods: {
     fetchRick() {
-      axios.get(API).then((response) => {
-        this.rickAPI = response.data;
+      let newAPI = API + "?&name="+this.rickName;
+      axios.get(newAPI).then((response) => {
+        this.rickAPI = response.data.results;
+        console.log(response.data)
       })
     }
   }
@@ -23,11 +26,11 @@ export default {
 
 <template>
   <div class="greetings">
-    <input v-model="message" placeholder="edit me" />
-    <button @click="fetchRick">Fetch</button>
+          <input v-model="rickName" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Search">
+    <button @click="fetchRick" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Fetch</button>
   </div>
-  <div v-if="rickAPI">
-    <div v-for="rick in rickAPI" :key="rick.name">
+  <div v-if="rickAPI" class="grid grid grid-cols-4 gap-4">
+    <div v-for="rick in rickAPI" :key="rick.ID" class="">
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
   <img class="w-full" v-bind:src="rick.image" alt="Sunset in the mountains">
   <div class="px-6 py-4">
@@ -37,9 +40,9 @@ export default {
     </p>
   </div>
   <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{rick.gender}}</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{rick.species}}</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{rick.location.name}}</span>
   </div>
 </div>
   </div>
