2b5f91e5def6d9c14541b05acee3e9fe070a7ab4
Fakhrurazi <fakhru@devstack.com.my>
2022-04-02 00:46:41 +0800

    First Push

---
+++ src/components/Rick.vue
@@ -0,0 +1,58 @@
+
+<script>
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+let API = 'https://rickandmortyapi.com/api/character';
+export default {
+  data() {
+    return {
+      message: 'Hello World!',
+      Rick: 'Rickk',
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('')
+    },
+    fetchRick() {
+      alert('asd');
+      axios.get(API).then((response) => {
+        console.log(response.data)
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <input v-model="message" placeholder="edit me" />
+    <button @click="fetchRick">Fetch</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+const apps = createApp(App).mount('#app')
