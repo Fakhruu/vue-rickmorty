ac8aa2c69199ce733b88220ea2797b82fa88593e
Fakhrurazi <fakhru@devstack.com.my>
2022-04-02 01:03:04 +0800

    Add Tailwind.

--- src/components/Rick.vue
+++ src/components/Rick.vue
@@ -4,22 +4,17 @@ import * as Vue from 'vue' // in Vue 3
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-let API = 'https://rickandmortyapi.com/api/character';
+let API = 'https://rickandmortyapi.com/api/character/2,435,13,44,112';
 export default {
   data() {
     return {
-      message: 'Hello World!',
-      Rick: 'Rickk',
+      rickAPI: false,
     }
   },
   methods: {
-    reverseMessage() {
-      this.message = this.message.split('').reverse().join('')
-    },
     fetchRick() {
-      alert('asd');
       axios.get(API).then((response) => {
-        console.log(response.data)
+        this.rickAPI = response.data;
       })
     }
   }
@@ -31,28 +26,23 @@ export default {
     <input v-model="message" placeholder="edit me" />
     <button @click="fetchRick">Fetch</button>
   </div>
+  <div v-if="rickAPI">
+    <div v-for="rick in rickAPI" :key="rick.name">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" v-bind:src="rick.image" alt="Sunset in the mountains">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">{{rick.name}}</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+  </div>
+  </div>
+
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import * as Vue from 'vue' // in Vue 3
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import './index.css'
+
 const apps = createApp(App).mount('#app')
